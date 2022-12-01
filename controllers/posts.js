7d79be965c0270ff2263d3beb12c7f27f444704a
RonNguyen166 <ronnguyen165>
2022-12-01 22:40:22 +0700

    update

--- controllers/posts.js
+++ controllers/posts.js
@@ -19,7 +19,7 @@ export const createPost = async (req, res) => {
     });
     await newPost.save();
 
-    const post = await Post.find().sort("-createdAt");
+    const post = await Post.find({}).sort("-createdAt");
     res.status(201).json(post);
   } catch (err) {
     res.status(409).json({ message: err.message });
@@ -29,7 +29,7 @@ export const createPost = async (req, res) => {
 /* READ */
 export const getFeedPosts = async (req, res) => {
   try {
-    const post = await Post.find().sort("-createdAt");
+    const post = await Post.find({}).sort("-createdAt");
     res.status(200).json(post);
   } catch (err) {
     res.status(404).json({ message: err.message });
